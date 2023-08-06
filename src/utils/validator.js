7a6ce5dea3c4d381000610d93dee41c042151438
Sprof22 <richmondelaigwu@gmail.com>
2023-08-06 18:41:23 +0100

    validator js

---
+++ src/utils/validator.js
@@ -0,0 +1,24 @@
+import Validator from 'validatorjs';
+
+let data = {
+  name: 'John',
+  email: 'johndoe@gmail.com',
+  age: 28
+};
+
+let rules = {
+  name: 'required',
+  email: 'required|email',
+  age: 'min:18'
+};
+
+let validation = new Validator(data, rules);
+
+// Check if the validation passes
+let isValidationPassed = validation.passes(); // true
+
+// Check if the validation fails
+let isValidationFailed = validation.fails(); // false
+
+console.log("Is validation passed?", isValidationPassed);
+console.log("Is validation failed?", isValidationFailed);
