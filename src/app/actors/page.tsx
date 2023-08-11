7ce88bdcaecbed36767fe01c15008cb60fb61937
Sprof22 <richmondelaigwu@gmail.com>
2023-08-11 11:21:12 +0100

    actors fix

---
+++ src/app/actors/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ActorsList from "../../components/ActorsList";
+
+const ActorsPage = () => {
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <ActorsList />
+    </div>
+  );
+};
+
+export default ActorsPage;
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,13 +6,12 @@ import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom'; // Imp
 const searchClient = algoliasearch('SHITSH8101', 'c1a6c821d09f58e251b49b035f7f8fd8');
 
 function App() {
-  const [query, setQuery] = useState(''); // State to store the search query
 
   return (
     <InstantSearch searchClient={searchClient} indexName="actor_index">
       <SearchBox
-        onChange={(event: any) => setQuery(event.currentTarget.value)} // Update query state on input change
-        // value={query}
+      className="p-3 shadow-sm relative block w-full pl-9 pr-3 py-2 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md focus:ring-1 focus:outline-none"
+
       />
       <Hits hitComponent={HitItem} /> {/* Render the search results */}
     </InstantSearch>
@@ -32,3 +31,5 @@ function HitItem({ hit }: any) {
 }
 
 export default App;
+
+
